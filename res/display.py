aa5113f21daed515d05fd9f7e8e0dea77cee5604
ilicid <lucaslauritsen29@gmail.com>
2026-04-14 09:50:24 +0200

    hg

---
+++ res/display.py
@@ -0,0 +1,20 @@
+import pygame
+
+width, height = (900,900)
+
+# set display mode with vsync enabled
+screen = pygame.display.set_mode((width, height), vsync=1)
+
+background_surface = pygame.Surface((width, height)).convert()
+background_surface.fill((3, 69, 28))
+
+fps = 60
+
+def display():
+    pygame.Surface.blit(screen, background_surface, (0, 0))
+    
+def display_update():
+    # updates the display
+    pygame.display.update()
+    # sets max frame rate
+    pygame.time.Clock().tick(fps)
