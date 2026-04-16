7d271f43b1b21c75f69a705b63becc7908ec8825
ilicid <lucaslauritsen29@gmail.com>
2026-04-16 12:07:06 +0200

    more notes

--- project/res/__init__.py
+++ project/res/__init__.py
@@ -1,4 +1,4 @@
 # Define the __all__ variable
-__all__ = ["display","celest","stars"]
+__all__ = ["display","celest","stars", "core"]
 
-from . import display, celest, stars
+from . import display, celest, stars, core
